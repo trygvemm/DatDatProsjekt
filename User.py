69a0dc159dffb47262fa7e373d3f802ac67eee38
Trygve Myhr <99827080+trygvemm@users.noreply.github.com>
2022-03-21 20:38:31 +0100

    Add files via upload

---
+++ User.py
@@ -0,0 +1,11 @@
+
+from msilib.schema import Class
+
+
+class User:
+
+    def __init__(self, mail, password, firstName, lastName):
+        self.mail = mail
+        self.password = password
+        self.firstName = firstName
+        self.lastName = lastName
