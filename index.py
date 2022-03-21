f5bf48c981ac88b13e9161c759110a486d908b95
trygvemm <trygvemm@stud.ntnu.no>
2022-03-21 20:29:17 +0100

    suppe

--- index.py
+++ index.py
@@ -1,18 +1,18 @@
 from tokenize import String
-from User import User
-from Post import Post
+#from User import User
+#from Post import Post
 import SQLindex
+
+userid = None
 
 
 def start():
     print("Velkommen til Kaffeapp\n")
     loglag = input("Logg inn skriv 1 \nLag bruker skriv 2")
-
     if loglag == "1":
         logIn()
     elif loglag == "2":
         makeUser()
-
 
 def makeUser():
     mail = input("Skriv inn mail: ")
@@ -23,7 +23,6 @@ def makeUser():
     SQLindex.insert_user(user)
     logIn()
 
-
 def logIn():
     print("LOGG INN:")
     mail = input("Skriv inn mail: ")
@@ -33,12 +32,21 @@ def logIn():
     if (dbPassword != None):
         if(dbPassword[0] == password):
             print("Du er logget inn")
+            menu()
         else:
             print("Feil passord")
             logIn()
     else:
         print(f"Ingen brukere med mail: {mail}")
         start()
+
+def menu():
+    choose = input("Lag Post: 1\nSe post: 2\nToppliste smakt flest kaffe: 3\nMest for pengene: 4\nFinn i beskrivelse: 5\nLand: 6\n")
+    if choose == "1":
+        post()
+    else:
+        print("under dev")
+        menu()
 
 
 start()
