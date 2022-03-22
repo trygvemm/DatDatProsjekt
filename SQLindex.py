3d874e478543a041f131c8351d8897a5563b1ca8
trygvemm <trygvemm@stud.ntnu.no>
2022-03-22 22:07:40 +0100

    Brukerhistorie 1-3.5 fixed

--- SQLindex.py
+++ SQLindex.py
@@ -4,8 +4,15 @@ from User import User
 from Post import Post
 
 import sqlite3
+def chk_conn(conn):
+     try:
+        conn.cursor()
+        return True
+     except Exception as ex:
+        return False
 connection = sqlite3.connect('coffee.db')
 cursor = connection.cursor()
+print(chk_conn(connection))
 
 #cursor.execute("INSERT INTO tabelnavn VALUES (tall, 'tekst', 'tekst')")
 #cursor.execute("UPDATE tabellnavn SET verdi = '' WHERE ID = tall")
@@ -19,30 +26,29 @@ def insert_user(user):
 def insert_post(post):
     with connection:
         cursor.execute("INSERT INTO Post (mail, coffeeID, note, score, date) VALUES (:mail, :coffeeID, :note, :score, :date)", {
-        'mail': "Sunil@gmail.com", 'coffeeID': post.coffeeID, 'note': post.note, 'score': post.score, 'date': post.date})
-
-def get_user_by_name(lastNamename):
-    cursor.execute(
-        "SELECT * FROM User WHERE lastName=:lastName", {'lastName': lastNamename})
-    return cursor.fetchall()
+        'mail': post.mail[0], 'coffeeID': post.coffeeID[0], 'note': post.note[0], 'score': post.score[0], 'date': post.date})
 
 def get_password(mail):
     cursor.execute(
         "SELECT password FROM User WHERE mail=:mail", {'mail': mail})
     return cursor.fetchone()
 
-def update_post(user, pay):
-    with connection:
-        cursor.execute("""UPDATE employees SET pay = :pay
-                    WHERE firstName = :firstName AND lastName = :lastName""",
-                       {'firstName': user.firstName, 'lastName': user.lastName, 'pay': pay})
-
 def getCoffeeID(Cname, Rname):
     cursor.execute(
         "SELECT coffeeID From Coffee WHERE coffeeName = :Cname AND roasteryID = (SELECT roasteryID from CoffeeRoastery WHERE name = :name)", {'Cname': Cname, 'name': Rname})
     return cursor.fetchone()
 
-def remove_user(user):
-    with connection:
-        cursor.execute("DELETE from employees WHERE firstName = :firstName AND lastName = :lastName",
-                       {'firstName': user.firstName, 'lastName': user.lastName})
+def get_mostcoffee():
+    cursor.execute(
+    "SELECT User.firstName, COUNT(DISTINCT Post.coffeeID) AS total from Post Inner join User on Post.mail = User.mail group by User.firstName ORDER BY total DESC;")
+    return cursor.fetchall()
+
+def get_mostvalue():
+    cursor.execute(
+    "SELECT CoffeeRoastery.name, Coffee.coffeeName, Coffee.priceKG, AVG(Post.score), Coffee.priceKG/AVG(Post.score) AS prisperpoeng from Post INNER JOIN Coffee ON Coffee.coffeeID = Post.coffeeID INNER JOIN CoffeeRoastery ON CoffeeRoastery.roasteryID = Coffee.roasteryID GROUP BY Coffee.coffeeName ORDER BY prisperpoeng;")
+    return cursor.fetchall()
+
+def get_search(search):
+    cursor.execute(
+    "SELECT Coffee.coffeeName, CoffeeRoastery.name FROM Coffee INNER JOIN CoffeeRoastery ON Coffee.roasteryID = CoffeeRoastery.roasteryID INNER JOIN Post ON Post.coffeeID = Coffee.coffeeID WHERE Post.note LIKE '%:search%' OR Coffee.description = '%:search%'", {'search': search})
+    return cursor.fetchall()
--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from tokenize import String
 from datetime import date
 from User import User
 from Post import Post
+from prettytable import PrettyTable
 import SQLindex
 
 # tid
@@ -57,18 +58,24 @@ def menu():
     print("----------MENY----------")
     print(userid)
     choose = input(
-        "Lag Post: 1\nSe post: 2\nToppliste smakt flest kaffe: 3\nMest for pengene: 4\nFinn i beskrivelse: 5\nLand: 6\n")
+        "Lag Post: 1\nListe over hvem som har smakt flest kaffer: 2\nBest kaffe for pengene: 3\nSøk i beskrivelse: 4\nLand: 5\nEXIT: 6\n")
     if choose == "1":
         makePost()
-    if choose == "2":
-        seePost()
+    elif choose == "2":
+        topList()
+    elif choose == "3":
+        mostValue()
+    elif choose == "4":
+        search()
+    elif choose == "6":
+        exit()
     else:
         print("under dev")
         menu()
 
 def makePost():
     print("----------LAG POST----------")
-    userid = 'Sunil@gmail.com'
+    userid = 'Trygve@gmail.com'
     coffee = input("Skriv inn kaffenavn: ")
     roastery = input("Skriv inn brennerinavn: ")
     score = int(input("Skriv inn score (1-10): "))
@@ -84,7 +91,36 @@ def makePost():
         print("Feil verdier for kaffenavn eller kaffebrenneri")
         makePost()
 
-def seePost():
-    print("----------SE POST----------")
+def topList():
+    print("----------TOPPLISTE----------")
+    #så langt i år????????
+    list = SQLindex.get_mostcoffee()
+    PT = PrettyTable()
+    PT.field_names = ["Fornavn","Antall smakt kaffer"]
+    for i in range(len(list)):
+        PT.add_row(list[i])
+    print(PT)
+    menu()
+
+def mostValue():
+    print("----------BEST KAFFE FOR PENGENE----------")
+    list = SQLindex.get_mostvalue()
+    PT = PrettyTable()
+    PT.field_names = ["Kaffenavn","Brennerinavn","Pris/kg","Gjennomsnittscore","Pris/poeng"]
+    for i in range(len(list)):
+        PT.add_row(list[i])
+    print(PT)
+    menu()
+
+def search():
+    print("----------SØK ETTER KAFFEBESKRIVELSE----------")
+    usr = input("Søk: ")
+    list = SQLindex.get_search(usr)
+    PT = PrettyTable()
+    PT.field_names = ["Brennerinavn","Kaffenavn"]
+    for i in range(len(list)):
+        PT.add_row(list[i])
+    print(PT)
+    menu()
 
 start()
